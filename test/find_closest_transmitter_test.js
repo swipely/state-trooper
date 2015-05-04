3d7a4ed8fbc47cc3566bbea9c2556055c4f0ab97
Simon Højberg <r.hackr@gmail.com>
2015-05-04 22:37:40 +0200

    Add initialFetch: false for better fetch control

--- test/find_closest_transmitter_test.js
+++ test/find_closest_transmitter_test.js
@@ -1,9 +1,9 @@
 "use strict";
 
 const expect = require('expect.js');
-const findClosestPersister = require('../lib/find_closest_persister');
+const findClosestTransmitter = require('../lib/find_closest_transmitter');
 
-describe('findClosestPersister', function () {
+describe('findClosestTransmitter', function () {
   var dataStore, path;
 
   describe('with a persister specified at the top level of the hierarchy', function () {
@@ -15,10 +15,10 @@ describe('findClosestPersister', function () {
       };
     });
 
-    it('can find that persister given a path at a lower level of the hierarchy', function () {
+    it('can find the persister given a path at a lower level of the hierarchy', function () {
       path = 'foo.bar.baz.what';
 
-      expect(findClosestPersister(dataStore, path)).to.eql('mock persister value');
+      expect(findClosestTransmitter('persister', dataStore, path)).to.eql('mock persister value');
     });
   });
 
@@ -31,10 +31,10 @@ describe('findClosestPersister', function () {
       };
     });
 
-    it('can find that persister given its exact path', function () {
+    it('can find the persister given its exact path', function () {
       path = 'foo.bar';
 
-      expect(findClosestPersister(dataStore, path)).to.eql('another mock persister value');
+      expect(findClosestTransmitter('persister', dataStore, path)).to.eql('another mock persister value');
     });
   });
 
--- test/state_trooper_test.js
+++ test/state_trooper_test.js
@@ -75,7 +75,6 @@ describe('StateTrooper', function () {
         });
       });
     });
-
   });
 
 });
