54995ffd801b4a27c35ede1c6d007f8d2a013ff1
Simon Højberg <r.hackr@gmail.com>
2015-11-22 17:50:46 +0100

    Add crude support for GraphQL queries

---
+++ src/find_closest_fetcher_and_query.js
@@ -0,0 +1,26 @@
+function findClosestFetcherAndQuery(type, dataStore, path) {
+  let pathStr = path.join('.');
+
+  if (!dataStore) {
+    throw new Error(
+      `Couldn't find fetcher for ${pathStr} with no data store`
+    );
+  }
+
+  while (!(dataStore[pathStr])) {
+    let parts = pathStr.split('.');
+    parts.pop();
+    pathStr = parts.join('.');
+
+    if (pathStr.length === 0) {
+      throw new Error(`Couldn't find fetcher for ${pathStr}`);
+    }
+  }
+
+  return {
+    fetcher: dataStore[pathStr].fetcher,
+    query: dataStore[pathStr].query
+  };
+}
+
+export default findClosestFetcherAndQuery;
