7c9bc5b9b2a02b19a5ebab0b771903e67d2ca737
Andrew Goodale <andrewgoodale@upserve.com>
2017-05-09 10:05:52 -0400

    TypeScript declarations

---
+++ index.d.ts
@@ -0,0 +1,49 @@
+// Type definitions for state-trooper 2.0
+// Project: state-trooper
+// Definitions by: Andrew Goodale <andrewgoodale@upserve.com>
+
+export interface Cursor<T> {
+  readonly path: string[];
+
+  deref(): T;
+  replace(val: T): void;
+  set(val: Partial<T>): void;
+  add(): void
+  remove(): void
+
+  refine<U>(path: string): Cursor<U>;
+
+  equals(other: any): boolean;
+
+  fetch(): void;
+  persist(): void;
+}
+
+export type QueryDef = string | object
+
+export interface DataStore {
+    readonly fetcher?: (cursor: Cursor<any>, rootCursor: Cursor<any>, query?: QueryDef) => void;
+    readonly persister?: (cursor: Cursor<any>, update: StateUpdate, rootCursor: Cursor<any>) => void;
+    readonly initialFetch?: boolean;
+    readonly query?: QueryDef;
+}
+
+export interface StateDescriptor<T> {
+    readonly state: T;
+    readonly dataStore?: { [key: string]: DataStore };
+    readonly stakeout?: { [key: string]: Array<StakeoutHandler> };
+}
+
+export type UpdateAction = 'set' | 'replace' | 'add' | 'remove';
+
+export interface StateUpdate {
+    readonly path: string[];
+    readonly action: UpdateAction;
+    readonly value: any;
+}
+
+export type StakeoutHandler = (update: StateUpdate, rootCursor: Cursor<any>) => void;
+
+export function patrol<T>(stateDesc: StateDescriptor<T>): any;
+
+export function stakeout(path: string, handler: StakeoutHandler): void;
